810aff32ef3410a8e08b0650077073ba9457dbae
Black-Lemonade <yuval.green@gmail.com>
2023-08-21 16:46:37 +0300

    functions add for test in app.js

--- src/app.js
+++ src/app.js
@@ -5,5 +5,20 @@ async function addSticky() {
 
   await miro.board.viewport.zoomTo(stickyNote);
 }
+async function addStar() {
+  const shape = await miro.board.createShape({
+    content: "<p>This is a very yellow star shape.</p>",
+    shape: "star",
+    style: {
+        fillColor: "#FEFF45",
+    },
+    x: 3000,
+    y: 4500,
+    width: 280,
+    height: 280,
+});
+}
+
 
 addSticky();
+addStar();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-export async function init() {
-  miro.board.ui.on('icon:click', async () => {
-    await miro.board.ui.openPanel({url: 'app.html'});
+async function init() {
+  // Enable the 'icon:click' event on the app icon
+  await miro.board.ui.on("icon:click", async () => {
+    // In this example: when the app icon is clicked, a method opens a panel
+    await miro.board.ui.openPanel({
+      // The content displayed on the panel is fetched from the specified HTML resource
+      url: "app.html"
+    });
   });
 }
 
